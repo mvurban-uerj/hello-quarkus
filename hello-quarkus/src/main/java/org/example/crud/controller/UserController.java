ddfa77dde75d4624115724403f5c481de4343251
mvurban <mvurban@mvurban.eti.br>
2025-11-25 21:09:48 +0000

    ajuste de git

--- hello-quarkus/src/main/java/org/example/crud/controller/UserController.java
+++ hello-quarkus/src/main/java/org/example/crud/controller/UserController.java
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-package org.example.crud.controller;
-
-import java.util.List;
-
-import org.example.crud.entity.UserEntity;
-import org.example.crud.service.UserService;
-
-import jakarta.transaction.Transactional;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DefaultValue;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-
-@Path("/users")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
-public class UserController {
-   
-   private final UserService userService;
-
-   public UserController(UserService userService) {
-      this.userService = userService;
-   }
-
-
-   @GET
-   public Response findAll(@QueryParam("page") @DefaultValue("0") int page,
-                           @QueryParam("pageSize") @DefaultValue("10") int pageSize ) {      
-       List<UserEntity> users = userService.findAll(page, pageSize);
-       return Response.ok(users).build();
-   }
-
-   @POST
-   @Transactional
-   public Response createUser(UserEntity userEntity) {      
-      return Response.ok(userService.createUser(userEntity)).build();
-   }
-
-}
-=======
 package org.example.crud.controller;
 
 import java.util.List;
@@ -126,4 +80,3 @@ public class UserController {
 
 
 }
->>>>>>> 11870f55f7aca39ff15b7c9d31f7d4532150c874
--- hello-quarkus/src/main/java/org/example/crud/service/UserService.java
+++ hello-quarkus/src/main/java/org/example/crud/service/UserService.java
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-package org.example.crud.service;
-import org.example.crud.entity.UserEntity;
-import jakarta.enterprise.context.ApplicationScoped;
-import java.util.List;
-
-@ApplicationScoped
-public class UserService {
- 
-   public UserEntity createUser(UserEntity userEntity){
-
-      UserEntity.persist(userEntity);
-      return userEntity;
-
-   }
-
-   public List<UserEntity> findAll(int page, int pageSize) {
-      return UserEntity.findAll()
-            .page(page, pageSize)
-            .list();
-   }
-
-}
-=======
 package org.example.crud.service;
 
 import java.util.List;
@@ -65,4 +41,3 @@ public class UserService {
     }
 
 }
->>>>>>> 11870f55f7aca39ff15b7c9d31f7d4532150c874
