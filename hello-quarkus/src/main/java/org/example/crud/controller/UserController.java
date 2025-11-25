11870f55f7aca39ff15b7c9d31f7d4532150c874
Marcelo Urban <mvurban@mvurban.eti.br>
2025-11-25 16:48:10 +0000

    finalização das funções do CRUD

--- hello-quarkus/src/main/java/org/example/crud/controller/UserController.java
+++ hello-quarkus/src/main/java/org/example/crud/controller/UserController.java
@@ -1,16 +1,20 @@
 package org.example.crud.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.example.crud.entity.UserEntity;
 import org.example.crud.service.UserService;
 
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
@@ -27,7 +31,23 @@ public class UserController {
       this.userService = userService;
    }
 
+   //CREATE USER   
+   @POST
+   @Transactional
+   public Response createUser(UserEntity userEntity) {      
+      return Response.ok(userService.createUser(userEntity)).build();
+   }
 
+   //Update User
+   @PUT
+   @Path("/{id}")
+   @Transactional
+   public Response updateUser(@PathParam("id") UUID id, UserEntity userEntity) {      
+      return Response.ok(userService.updateUser(id, userEntity)).build();
+   }  
+
+
+   //FIND ALL WITH PAGINATION
    @GET
    public Response findAll(@QueryParam("page") @DefaultValue("0") int page,
                            @QueryParam("pageSize") @DefaultValue("10") int pageSize ) {      
@@ -35,10 +55,28 @@ public class UserController {
        return Response.ok(users).build();
    }
 
-   @POST
-   @Transactional
-   public Response createUser(UserEntity userEntity) {      
-      return Response.ok(userService.createUser(userEntity)).build();
+   //FIND BY ID
+   @GET
+   @Path("/{id}")
+   public Response findById(@PathParam("id") UUID id) {      
+      UserEntity user = userService.findById(id);
+      return Response.ok(user).build();
    }
+
+   //DELETE USER 
+   @DELETE
+   @Path("/{id}")
+   @Transactional
+   public Response deleteUser(@PathParam("id") UUID id) {      
+      
+      userService.deleteUser(id);
+      return Response.noContent().build();
+      
+   }
+
+
+
+
+
 
 }
--- hello-quarkus/src/main/java/org/example/crud/service/UserService.java
+++ hello-quarkus/src/main/java/org/example/crud/service/UserService.java
@@ -1,22 +1,43 @@
 package org.example.crud.service;
-import org.example.crud.entity.UserEntity;
-import jakarta.enterprise.context.ApplicationScoped;
+
 import java.util.List;
+import java.util.UUID;
+
+import org.example.crud.entity.UserEntity;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.WebApplicationException;
 
 @ApplicationScoped
 public class UserService {
- 
-   public UserEntity createUser(UserEntity userEntity){
 
-      UserEntity.persist(userEntity);
-      return userEntity;
+    public UserEntity createUser(UserEntity userEntity) {
+        UserEntity.persist(userEntity);
+        return userEntity;
+    }
 
-   }
+    public UserEntity updateUser(UUID id, UserEntity userEntity) {
+        UserEntity existingUser = (UserEntity) UserEntity.findByIdOptional(id).orElseThrow(() -> new WebApplicationException("User not found", 404));
+        existingUser.userName = userEntity.userName;
+        UserEntity.persist(existingUser);
+        return existingUser;
+    }
 
-   public List<UserEntity> findAll(int page, int pageSize) {
-      return UserEntity.findAll()
-            .page(page, pageSize)
-            .list();
-   }
+    public List<UserEntity> findAll(int page, int pageSize) {
+        return UserEntity.findAll()
+                .page(page, pageSize)
+                .list();
+    }
+
+    public UserEntity findById(UUID id) {
+
+        return (UserEntity) UserEntity.findByIdOptional(id).orElseThrow(() -> new WebApplicationException("User not found", 404));
+
+    }
+
+    public void deleteUser(UUID id) {
+        UserEntity userEntity = (UserEntity) UserEntity.findByIdOptional(id).orElseThrow(() -> new WebApplicationException("User not found", 404));
+        UserEntity.delete("userId", userEntity.userId);
+    }
 
 }
